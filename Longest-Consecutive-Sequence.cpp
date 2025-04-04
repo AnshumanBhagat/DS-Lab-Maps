b1ddae7e145ae46d4cdcd29e168118b95c1a6ff4
AnshumanBhagat <166050966+AnshumanBhagat@users.noreply.github.com>
2025-04-04 23:34:06 +0530

    Longest-Consecutive-Sequence.cpp

---
+++ Longest-Consecutive-Sequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+         if (nums.empty()) return 0;
+        
+        map<int, int> mpp;
+        for(int i = 0; i<nums.size(); i++){
+            mpp[nums[i]] = i;
+        }
+        
+        auto it = mpp.begin();
+        int start = it->first;   
+        it++;
+        int ans = 1, cnt = 1;
+        while(it != mpp.end()){
+            if(it->first == start+1){
+                cnt++;
+            }
+
+            else{
+                cnt = 1;
+            }
+            ans = max(cnt, ans);
+            start = it->first;
+            it++;
+        }
+        return ans;  
+    }
+};
